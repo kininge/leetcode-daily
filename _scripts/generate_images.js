216fa44e674ea6a8f4e116c592b856aec0d9e2c9
Pritam Sukumar Kininge <kininge@users.noreply.github.com>
2025-08-14 20:10:52 +0530

    Update generate_images.js

--- _scripts/generate_images.js
+++ _scripts/generate_images.js
@@ -2,39 +2,37 @@ const fs = require("fs");
 const path = require("path");
 
 // Load problem data
-const dataPath = path.join(__dirname, "../data.json");
-const { totalSolved, easy, medium, hard } = require(dataPath);
+const dataPath = path.join(__dirname, "./data.json");
 
-// SVG size
-const width = 400;
-const barWidth = 300;
-const barHeight = 20;
-const barX = 50;
-const barY = 150;
 
-// Calculate widths
-const easyW = (easy / totalSolved) * barWidth;
-const medW = (medium / totalSolved) * barWidth;
-const hardW = (hard / totalSolved) * barWidth;
+const { totalCardWidth, totalCardHeight, totalCardBorderRadius, totalCardBackgroundColor, totalSolvedProblmes, primaryTextColor, secondaryTextColor, 
+easyBackgroundColor, mediumBackgroundColor, hardBackgroundColor, solvedEasyProblems, solvedMediumProblems, solvedHardProblems, 
+totalCardEachBarBorderRadius, totalCardPrimaryTextFontSize, totalCardSecondaryTextFontSize } = require(dataPath);
 
-// Generate SVG
-const svg = `
-<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="220" viewBox="0 0 ${width} 220">
-  <rect x="0" y="0" width="${width}" height="220" rx="8" fill="#282828" />
+// generate total card
+const availabaleTotalCardWidth = totalCardWidth-40;
+const easyBarWidth = (solvedEasyProblems/totalSolvedProblmes)*availabaleTotalCardWidth;
+const mediumBarWidth = (solvedMediumProblems/totalSolvedProblmes)*availabaleTotalCardWidth;
+const hardBarWidth = (solvedHardProblems/totalSolvedProblmes)*availabaleTotalCardWidth;
+
+const totalCardImage = `<svg xmlns="http://www.w3.org/2000/svg" width="${totalCardWidth}" height="${totalCardHeight}" viewBox="0 0 ${totalCardWidth} ${totalCardHeight}">
+  <!-- card background -->
+  <rect x="0" y="0" width="${totalCardWidth}" height="${totalCardHeight}" rx="${totalCardBorderRadius}" fill="${totalCardBackgroundColor}" />
   
-  <!-- Problem solved numbers -->
+  <!-- All problem solved data -->
   <g transform="translate(160, 120)" text-anchor="middle">
-    <text font-size="70" font-weight="600" font-family="Inter, Arial, Helvetica, sans-serif" fill="#ffffff" dominant-baseline="end">${totalSolved}</text>
-    <text font-size="25" font-weight="600" font-family="Inter, Arial, Helvetica, sans-serif" fill="#868686" dominant-baseline="end" dx="80">/ 3647</text>
+    <!-- total problems solved -->
+    <text font-size="${totalCardPrimaryTextFontSize}" font-weight="600" font-family="Inter, Arial, Helvetica, sans-serif" fill="${primaryTextColor}" dominant-baseline="end">${totalSolvedProblmes}</text>
+    <!-- total problems -->
+    <text font-size="${totalCardSecondaryTextFontSize}" font-weight="600" font-family="Inter, Arial, Helvetica, sans-serif" fill="${secondaryTextColor}" dominant-baseline="end" dx="80">/ ${totalProblems}</text>
   </g>
 
-  <!-- Progress bar -->
-  <rect x="${barX}" y="${barY}" width="${easyW}" height="${barHeight}" fill="#4CAF50" />
-  <rect x="${barX + easyW}" y="${barY}" width="${medW}" height="${barHeight}" fill="#FFC107" />
-  <rect x="${barX + easyW + medW}" y="${barY}" width="${hardW}" height="${barHeight}" fill="#F44336" />
-</svg>
-`;
+  <!-- horizontal bar represent each section problem solved -->
+  <g transform="translate(16, 184)">
+    <rect x="0" y="0" width="${easyBarWidth}" height="20" fill="${easyBackgroundColor}" rx="${totalCardEachBarBorderRadius}" /> 
+    <rect x="${easyBarWidth+4}" y="0" width="${mediumBarWidth}" height="20" fill="${mediumBackgroundColor}" rx="${totalCardEachBarBorderRadius}" />
+    <rect x="${easyBarWidth+mediumBarWidth+8}" y="0" width="${hardBarWidth}" height="20" fill="${hardBackgroundColor}" rx="${totalCardEachBarBorderRadius}" />
+  </g>
+</svg>`
 
-// Save SVG
-fs.writeFileSync(path.join(__dirname, "../leetcodeCard.svg"), svg);
-console.log("✅ LeetCode card generated!");
+fs.writeFileSync(path.join(__dirname, "../assets/totalCard.svg"), totalCardImage);
