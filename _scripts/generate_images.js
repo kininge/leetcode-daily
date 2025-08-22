df7e23075bae4d91538bfa7f1cc248a826faa916
Pritam Sukumar Kininge <kininge@users.noreply.github.com>
2025-08-22 10:04:12 +0530

    Update generate_images.js

--- _scripts/generate_images.js
+++ _scripts/generate_images.js
@@ -66,7 +66,7 @@ const mediumCardImage = `<svg xmlns="http://www.w3.org/2000/svg" width="${sectio
   <!-- card title -->
   <text x="16" y="28" font-size="${sectionTitleTextFontSize}" font-weight="600" font-family="${fontfamily}" fill="${mediumTextColor}">Medium</text>
   
-  <g transform="translate(70, 90)" text-anchor="middle">
+  <g transform="translate(65, 90)" text-anchor="middle">
     <!-- section problems solved -->
     <text font-size="${sectionCardPrimaryTextFontSize}" font-weight="600" font-family="${fontfamily}" fill="${primaryTextColor}" dominant-baseline="end">${solvedMediumProblems}</text>
     <!-- section problems available -->
